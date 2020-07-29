bdd07fbc8f9eaf7078cb58f36695b8a89b2e1421
Ken Cheng <kenfcheng@gmail.com>
2020-07-29 11:35:42 -0400

    Started Get (app.get)

--- Develop/index.js
+++ Develop/index.js
@@ -5,3 +5,25 @@ const fs = require("fs");
 const app = express();
 const port = 8080;
 const mainDir = path.join(__dirname, "/public");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/* GET: Reads 'db.json' file and return all saved notes as JSON */
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function (req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
